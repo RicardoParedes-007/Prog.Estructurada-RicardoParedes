b9094dcda8a8f25064d1041a91f821936ca68c18
Ricardo Paredes <ricardo.paredes8698@alumnos.udg.mx>
2025-01-30 15:35:48 -0600

    Add files via upload
    Un código sencillo que imprime una serie de datos en la pantalla

---
+++ datos-del-usuario.cpp
@@ -0,0 +1,19 @@
+//PRESENTACION ACTIVIDAD DE PROGRAMACION 1
+//NOMBRE: RICARDO PAREDES SANCHEZ
+//FECHA: 30/ENERO/2025
+//PROGRAMA: IMPRESION DE DATOS
+//CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+//INGENIERIA EN COMPUTACION / 2do SEMESTRE
+//PROFESOR: Sergio Franco Casillas
+//DESCRIPCION: ESTE PROGRAMA RECOLECTA LA INFORMACION DEL USUARIO Y LA MUESTRA EN PANTALLA
+#include <stdio.h>
+int main() 
+{
+printf("Nombre: Ricardo Paredes Sanchez\n");
+printf("Edad: 18 años\n");
+printf("Lugar de residencia: Capilla de Guadalupe\n");
+printf("Fecha: 30 de Enero del 2025\n");
+printf("Carrera: Ingenieria en Computacion\n");
+printf("Semestre: Segundo Semestre\n");
+return 0;
+}
