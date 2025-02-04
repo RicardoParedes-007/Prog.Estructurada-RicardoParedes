4de24113610c2961022c039293378da196ac9b13
Ricardo Paredes <ricparsan@gmail.com>
2025-02-03 23:05:39 -0600

    commit de prueba 1

---
+++ holamundo-para-git-RicardoParedesSanchez.cpp
@@ -0,0 +1,12 @@
+/*RICARDO PAREDES SANCHEZ
+2 DE FEBRERO DE 2025
+HOLA MUNDO.CPP
+CENTRO UNIVERSITARIO DE LOS ALTOS/UNIVERSIDAD DE GUADALAJARA
+PROFESOR:SERGIO FRANCO CASILLAS
+DESCRIPCION:UN HOLA MUNDO HECHO CON EL UNICO PROPOSITO DE APRENDER A HACER UN PUSH EN GIT Y GITHUB*/
+#include <stdio.h>   //incluimos las bibliotecas necesarias
+int main()          //el int main marca el inicio del programa
+{
+    printf("hola mundo");  //el printf lo que hace es permitirnos mostrar palabras en la pantalla
+    return 0;    //el return 0 hace que el programa se acabe
+}
